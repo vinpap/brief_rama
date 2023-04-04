41ab1051a25cd95f8a0ac70df3c672a2d967b6b7
JoaoAndre-RosaTeixeira <91742556+JoaoAndre-RosaTeixeira@users.noreply.github.com>
2023-04-04 16:50:00 +0200

    fix

--- script.js
+++ script.js
@@ -41,19 +41,19 @@ glaces = [
     glace_chocolat = new Glace(
         "Chocolat",
         "Savourez chaque bouchée de notre glace artisanale au chocolat, faite avec les ingrédients les plus fins et les plus savoureux. Laissez-vous transporter par sa texture crémeuse et onctueuse qui fond en bouche. Une expérience glacée inoubliable pour les amateurs de chocolat.",
-        "chocolat.png"
+        "assets/img/chocolat.png"
     ),
 
     glace_vanille = new Glace(
         "Vanille",
         "Dégustez notre glace artisanale à la vanille, créée à partir de gousses de vanille de qualité supérieure pour une saveur riche et veloutée. Laissez-vous transporter par la douceur de sa texture crémeuse et onctueuse qui fond en bouche. Une expérience glacée délicieusement classique pour les amateurs de vanille.",
-        "vanille.png"
+        "assets/img/vanille.png"
     ),
 
     glace_fraise = new Glace(
         "Fraise",
         "Savourez notre glace artisanale à la fraise, préparée avec des fraises fraîches et juteuses pour une saveur fruitée et acidulée. Laissez-vous emporter par la douceur de sa texture crémeuse et onctueuse qui fond en bouche. Une expérience glacée irrésistiblement fruitée pour les amateurs de fraise.",
-        "fraise.png"
+        "assets/img/fraise.png"
     )
 ]
 
@@ -62,10 +62,15 @@ glaces[0].updateGlaceHTML();
 
 
 let currentIndex = 0;
+setTimeout(() => {
+    document.querySelector('.presentation .container').style.animation = "animation: 1s infinite alternate slidein;";
+}, 4000);
+
 setInterval(() => {
     currentIndex = (currentIndex + 1) % glaces.length;
-    glaces[currentIndex].update();
+    glaces[currentIndex].updateGlaceHTML();
 }, 5000);
+
 
 document.getElementById('toggleContactBtn').addEventListener('click', function () {
     const contactSection = document.querySelector('.contact');
